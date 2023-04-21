acb9aea98bba92727d53523ec70367e859bee115
imaglide <imaglide@gmail.com>
2023-04-21 15:21:41 -0400

    Add files via upload

---
+++ openaiqbfrontendv2/src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+import OpenAIInput from "./components/OpenAIInput";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <OpenAIInput />
+      </header>
+    </div>
+  );
+}
+
+export default App;
